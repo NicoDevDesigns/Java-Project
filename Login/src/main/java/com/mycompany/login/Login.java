0246c2dde0acc12c8bd5bb241bb7461525db5dd8
Sanchez Nicolas <sancheznicolasalejandro@gmail.com>
2023-05-30 16:52:38 +0200

    proyectos mayo 2023

---
+++ Login/src/main/java/com/mycompany/login/Login.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.login;
+
+import com.mycompany.login.igu.Principal;
+
+
+public class Login {
+
+    public static void main(String[] args) {
+        Principal objIgu = new Principal();
+        objIgu.setVisible(true);
+        objIgu.setLocationRelativeTo(null); 
+    }
+}
---
+++ Login/src/main/java/com/mycompany/login/persistente/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.mycompany.login.persistente.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
